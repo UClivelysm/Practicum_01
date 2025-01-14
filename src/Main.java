e4034bd31ab174aa3072299345383eedde5e93e2
samuellively <livelysm@mail.uc.edu>
2025-01-14 13:48:39 -0500

    add safeinput.java 2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args)
-    {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello World!");
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String firstName = "";
+
+        firstName = SafeInput.getNonZeroLenString(in, "Enter your first name");
+        System.out.println("First name is " + firstName);
     }
 }
