0cd0728955e6ecd4d31c299e1ae619b13c98d8fd
UClivelysm <livelysm@mail.uc.edu>
2025-01-16 13:01:01 -0500

    Commit#2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,39 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        boolean doneInput = false;
+        String ID = "";
+        String firstName = "";
+        String lastName = "";
+        String title = "";
+        String rec = "";
+
+        int YOB = 0;
+
+        ArrayList <String> people = new ArrayList<>();
+
         Scanner in = new Scanner(System.in);
 
-        String firstName = "";
 
-        firstName = SafeInput.getNonZeroLenString(in, "Enter your first name");
-        System.out.println("First name is " + firstName);
+        //create a loop to input person data
+        do {
+            ID = SafeInput.getNonZeroLenString(in, "Enter Your ID [000001]");
+            firstName = SafeInput.getNonZeroLenString(in, "Enter your first name");
+            lastName = SafeInput.getNonZeroLenString(in, "Enter your last name");
+            title = SafeInput.getNonZeroLenString(in, "Enter your title");
+            YOB = SafeInput.getRangedInt(in, "Enter your year of birth", 1000, 9999);
+
+            rec = ID + ", " + firstName + ", " + lastName + ", " + title + ", " + YOB;
+            people.add(rec);
+
+            doneInput = SafeInput.getYNConfirm(in, "Are you done? [Y/N]");
+        }while (!doneInput);
+
+        System.out.println(rec);
     }
 }
